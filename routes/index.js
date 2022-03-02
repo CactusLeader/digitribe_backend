512266b74edb5a7462b24435cacf9569dd70bd0e
Elodie Morel <elodie.morel08@gmail.com>
2022-03-02 17:20:04 +0100

    fs en double supp

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-var fs = require("fs");
 
 const userModel = require("../models/users");
 const messageModel = require("../models/messages");
