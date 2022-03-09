7e7ef2553fa7c622e39198b7942d31175f57ea12
Maxime Gomez-Duret <maxime.gomezduret@gmail.com>
2022-03-09 17:55:22 +0100

    test update

--- routes/index.test.js
+++ routes/index.test.js
@@ -1,45 +1,30 @@
 const app = require("../app");
 const request = require("supertest");
 
-
-test("recup point", async (done) => {
-  const res = await request(app)
-    .get("/map")
-    .query({
-      coordigitnate: {
-        lat: 47.3,
-        lon: 6.2,
-      },
-    });
-  expect(typeof res.body.description).toEqual("string");
-  expect(typeof res.body.coordinate).toEqual("object");
-  done();
+test("recup point", async () => {
+  const res = await request(app).post("/map").send({
+    currentLongitude: 7.26,
+    currentLatitude: 43.7,
+  });
+  // expect(typeof res.body.description).toEqual("string");
+  expect(typeof res.body.places).toEqual("object");
 });
 
-test("lorsque la coordonnée est invalide", async (done) => {
+test("lorsque la coordonnée est invalide", async () => {
   await request(app)
-    .get("/map")
-    .query({
-      coordinate: null,
+    .post("/map")
+    .send({
+      currentLongitude: 0,
     })
     .expect({
       result: false,
     });
-  done();
 });
 
-test("quand personne ne se trouve à proximité", async (done) => {
-  await request(app)
-    .get("/map")
-    .query({
-      coordinate: {
-        lat: 43.6,
-        lon: 7.24,
-      },
-    })
-    .expect({
-      result: false,
-      error: "Personne ne se trouve à proximité",
-    });
-  done();
+test("quand personne ne se trouve à proximité", async () => {
+  const res = await request(app).post("/map").send({
+    currentLongitude: 0,
+    currentLatitude: 0,
+  });
+  expect(res.body.error).toEqual("Personne ne se trouve à proximité");
 });
